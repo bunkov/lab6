a3b200f81cd7b8312dadcd1f0f918d95c1f5f532
bunkov <bunk.alexey@gmail.com>
2015-12-06 23:35:27 +0300

    6 работа 2 часть сделана

--- A.py
+++ A.py
@@ -1,7 +1,14 @@
-n = int(input())
-a = list(map(int, input().split()))
+inp=open('input.txt')
+out=open('output.txt','w')
+s=inp.readline().rstrip()
+n=int(s)
+s=inp.readline().rstrip()
+a = list(map(int, s.split()))
 a = a[:n]
+
 for i in range(n-1):
 	for j in range(i+1, n):
 		if a[i] == a[j]:
-			print(a[i])
+			print(a[i],file=out)
+inp.close()
+out.close()
